b6321063c4984c80911c7eda913747d6b6f37186
Ethan Crann <ecrann@protonmail.com>
2024-07-09 02:46:33 -0230

    Doesn't work. Need Sleep.

--- src/react/carosel.tsx
+++ src/react/carosel.tsx
@@ -1,4 +1,4 @@
-import React, {createRef} from "react";
+import React, {createRef, useEffect, useRef} from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -8,8 +8,23 @@ const names = ["Growth Jam", "Gump Jam", "CEO Mindset", "Squid Jam"];
 
 
 export default function Carousel() {
-
+  const [lastSlide, setLastSlide] = React.useState(0);
   const [currentSlide, setCurrentSlide] = React.useState(0);
+
+  const vid0 = useRef<HTMLVideoElement>(null);
+  const vid1 = useRef<HTMLVideoElement>(null);
+  const vid2 = useRef<HTMLVideoElement>(null);
+  const vid3 = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (vid0.current !== null && vid1.current !== null && vid2.current !== null && vid3.current !== null) {
+      vid0.current.pause();
+      vid1.current.pause();
+      vid2.current.pause();
+      vid3.current.pause();
+    }
+  },[currentSlide]);
+
   const title = <h1 style={{margin: "20px"}}>{names[currentSlide]}</h1>
   let mobile = false;
   if (window.matchMedia("(max-width: 768px)").matches) {
@@ -17,34 +32,21 @@ export default function Carousel() {
     mobile = true;
   } 
 
-  const desktopSettings = {
+  const settings = {
     className: "center",
     adaptiveHeight: true,
     centerMode: true,
     centerPadding: "60px",
     speed: 500,
     slidesToShow: 1,
-    beforeChange: (current: number, next: number) => setCurrentSlide(next),
-  };
-  
-  const mobileSettings = {
-    vertical: true,
-    verticalSwiping: true,
-    speed: 500,
-    slidesToShow: 3,
-    infinite: true,
-    arrows: false,
-    beforeChange: (current: number, next: number) => setCurrentSlide(next),
+    beforeChange: (current: number, next: number) => {setCurrentSlide(next);},
   };
 
-  let settings = {};
   let videoDimensions = {width: 720, height: 480}
   if (mobile) {
-    settings = mobileSettings;
     videoDimensions = {width: 200, height: 200}
   }
   else {
-    settings = desktopSettings;
     videoDimensions = {width: 720, height: 480}
   }
 
@@ -80,22 +82,22 @@ export default function Carousel() {
     <div className="slider-container slider-wrapper">
     <Slider {...settings}>
       <div>
-          <video width={videoDimensions.width} height={videoDimensions.height} controls>
+          <video ref={vid0} width={videoDimensions.width} height={videoDimensions.height} controls>
             <source src="https://www.cs.mun.ca/~etcrann/GameJams/Growth.mp4" type="video/mp4"></source>
           </video>
       </div>
       <div>
-          <video width={videoDimensions.width} height={videoDimensions.height} controls>
+          <video ref={vid1} width={videoDimensions.width} height={videoDimensions.height} controls>
             <source src="https://www.cs.mun.ca/~etcrann/GameJams/GumpJam.mp4" type="video/mp4"></source>
           </video>
       </div>
       <div>
-          <video width={videoDimensions.width} height={videoDimensions.height} controls>
+          <video ref={vid2} width={videoDimensions.width} height={videoDimensions.height} controls>
             <source src="https://www.cs.mun.ca/~etcrann/GameJams/CEO.mp4" type="video/mp4"></source>
           </video>
       </div>
       <div>
-          <video width={videoDimensions.width} height={videoDimensions.height} controls>
+          <video ref={vid3} width={videoDimensions.width} height={videoDimensions.height} controls>
             <source src="https://www.cs.mun.ca/~etcrann/GameJams/CardJam.mp4" type="video/mp4"></source>
           </video>
       </div> 
